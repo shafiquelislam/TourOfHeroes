b452f135111925b4b1a0eb123f3bcb0c057c4f01
shafiqul <shafiqul@bitmascot.com>
2017-08-18 15:19:36 +0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HeroService]
 })
-export class AppComponent {
-  title = 'app';
+
+export class AppComponent implements OnInit{
+  title = 'Tour Of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+
+  constructor(private heroService: HeroService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then((heroes) => {
+      this.heroes = heroes;
+    });
+  }
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 95, name: 'Tamim' },
+  { id: 12, name: 'Imrul' },
+  { id: 1, name: 'Sabbir' },
+  { id: 75, name: 'Shakib' },
+  { id: 97, name: 'Mushfiq' },
+  { id: 30, name: 'Riad' },
+  { id: 10, name: 'Nasir' },
+  { id: 2, name: 'Mashrafee' },
+  { id: 19, name: 'Shafiul' },
+  { id: 20, name: 'Mustafiz' },
+  { id: 50, name: 'Taijul' }
+];
+
